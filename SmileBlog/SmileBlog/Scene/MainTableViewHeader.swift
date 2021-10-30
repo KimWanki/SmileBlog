195c8eb5313c1343184ba5f974c6c55c22cfb1a2
KimWanki <seoulkwk95@gmail.com>
2021-10-30 19:22:05 +0900

    feat: 메인화면 내 테이블 뷰 헤더 생성

---
+++ SmileBlog/SmileBlog/Scene/MainTableViewHeader.swift
@@ -0,0 +1,12 @@
+//
+//  MainTableViewHeader.swift
+//  SmileBlog
+//
+//  Created by WANKI KIM on 2021/10/30.
+//
+
+import UIKit
+
+class MainTableViewHeader: UITableViewHeaderFooterView {
+    
+}
