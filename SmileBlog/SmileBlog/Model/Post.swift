bfc9f73f073267e554151ca6ed5fc93463d02686
KimWanki <seoulkwk95@gmail.com>
2021-10-30 19:54:44 +0900

    feat: Post 모델 구현
    테이블 뷰 셀을 그리기 위한 Post 모델 구현

---
+++ SmileBlog/SmileBlog/Model/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  SmileBlog
+//
+//  Created by WANKI KIM on 2021/10/30.
+//
+
+import Foundation
+
+struct Post {
+    let title: String
+    let content: String
+    let date: String
+    let comments: Int
+}
