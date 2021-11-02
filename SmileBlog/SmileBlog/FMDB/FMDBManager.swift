66c47271d6b34de15fb465d81a989459ae5a4c60
KimWanki <seoulkwk95@gmail.com>
2021-11-02 21:52:38 +0900

    feat: 데이터베이스 구성을 sqlite 브릿징 및 FMDBManager 생성

---
+++ SmileBlog/SmileBlog/FMDB/FMDBManager.swift
@@ -0,0 +1,17 @@
+//
+//  FMDBManager.swift
+//  SmileBlog
+//
+//  Created by WANKI KIM on 2021/11/02.
+//
+
+import Foundation
+
+//class FMDBManager {
+//    static let shared = FMDBManager()
+//
+//    private init() { }
+//
+//    lazy var fmdb: FMDB
+//
+//}
