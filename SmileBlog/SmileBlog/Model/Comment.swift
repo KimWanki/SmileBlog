4eb2fcb8296d86f554b5c4e49f8acc3eab3e14ee
KimWanki <seoulkwk95@gmail.com>
2021-11-03 22:43:25 +0900

    feat: 댓글 기능을 위한 Comment 타입 구현

---
+++ SmileBlog/SmileBlog/Model/Comment.swift
@@ -0,0 +1,16 @@
+//
+//  Comment.swift
+//  SmileBlog
+//
+//  Created by WANKI KIM on 2021/11/03.
+//
+
+import Foundation
+
+struct Comment {
+    let number: Int?
+    let user: String
+    let post: Int
+    let content: String
+    let date: String
+}
